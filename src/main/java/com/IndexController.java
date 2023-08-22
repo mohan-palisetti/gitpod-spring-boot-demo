4b4b177ecdc8a4b16aa1ebc386faacd6ab4fad05
mohan-palisetti <mohan.palisetti@gmail.com>
2023-08-22 17:58:48 +0000

    initial commit

---
+++ src/main/java/com/IndexController.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World" + LocalDateTime.now();
+    }
+}
